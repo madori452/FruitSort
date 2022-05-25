a182e89779f2172067e8b52f62bbec20008c43e6
Tracy Tung <d820430@yahoo.com.tw>
2022-05-25 12:20:16 +0800

    fix js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const url = 'https://shannon945.github.io/farm_produce/data.json';
 let data = [];
+let filterData = [];
+const name = document.querySelector('#js-crop-name');
 const showList = document.querySelector('.showList');
 function init() {
   axios
     .get(url)
     .then(function(response) {
       data = response.data;
-      render();
     })
     .catch(function(error) {
       console.log(error);
@@ -34,53 +35,66 @@ function render(arr) {
 //資料篩選
 const btnGroup = document.querySelector('.button-group');
 btnGroup.addEventListener('click', dataFilter);
+const btn = document.querySelectorAll('.btn');
 
 function dataFilter(e) {
-  e.preventDefault;
   //btn 樣式
-  const btn = document.querySelectorAll('.btn');
-  btn.forEach(i => {
-    i.classList.remove('active');
-  });
-  e.target.classList.add('active');
-  //tab資料篩選
-  let filterData = [];
-  const get = e.target.getAttribute('data-type');
-  if (get == 'N05') {
-    filterData = data.filter(i => i.種類代碼 == 'N05');
-    console.log(filterData);
-  } else if (get == 'N04') {
-    filterData = data.filter(i => i.種類代碼 == 'N04');
-    console.log(filterData);
-  } else {
-    filterData = data.filter(i => i.種類代碼 == 'N06');
-    console.log(filterData);
+  if (e.target.nodeName === 'BUTTON') {
+    btn.forEach(i => {
+      i.classList.remove('active');
+    });
+    e.target.classList.add('active');
+    //tab資料篩選
+    const get = e.target.getAttribute('data-type');
+
+    if (get == 'N05') {
+      filterData = data.filter(i => i.種類代碼 == 'N05');
+    } else if (get == 'N04') {
+      filterData = data.filter(i => i.種類代碼 == 'N04');
+    } else {
+      filterData = data.filter(i => i.種類代碼 == 'N06');
+    }
+    render(filterData);
+    name.textContent = '';
+    Reset();
   }
-  render(filterData);
+}
+//清除欄位
+function Reset() {
+  let inputText = document.querySelector('.name').value;
+  inputText.length != 0 ? (inputText = '') : (inputText = inputText);
 }
 
 //資料搜尋
 const searchData = document.querySelector('.search');
 searchData.addEventListener('click', search);
+
 function search() {
-  let inputValue = document.querySelector('input.name').value;
-  let filterItem = [];
+  let inputValue = document.querySelector('input.name').value.trim();
   if (inputValue == '') {
     alert('請輸入資料');
     return;
   }
-  filterItem = data.filter(i => i.作物名稱.match(inputValue));
+  filterData = data.filter(i => i.作物名稱.match(inputValue));
   //找不到資料
-  if (filterItem.length == 0) {
+  if (filterData.length == 0) {
     showList.innerHTML = `<tr>
        <td colspan="7" class="text-center p-3">找不到資料</td>
       </tr>`;
   } else {
-    render(filterItem);
+    name.textContent = `查看「${inputValue}」的搜尋結果`;
+    render(filterData);
   }
+  restBtn();
+}
+//清除按鈕樣式
+function restBtn() {
+  btn.forEach(i => {
+    i.classList.remove('active');
+  });
 }
 
-//資料進階排序
+//表格(排序)
 const select = document.querySelector('.js-sort-advanced');
 
 select.addEventListener('click', sort01);
@@ -90,28 +104,26 @@ function sort01(e) {
     let sortStatus = e.target.dataset.sort;
     let sortPrice = e.target.dataset.price;
     if (sortStatus == 'up') {
-      // 大到小
-      data.sort((a, b) => {
-        return b[sortPrice] - a[sortPrice];
-      });
+      // 高到低
+      selectUP(sortPrice);
     } else if (sortStatus == 'down') {
-      // 小到大
-      data.sort((a, b) => {
-        return a[sortPrice] - b[sortPrice];
-      });
+      // 低到高
+      selectDown(sortPrice);
     }
-    render(data);
+    render(filterData);
   }
 }
-// select
+// 右側下拉選單(排序)
 const dropSelect = document.querySelector('#js-select');
+const dropSelectMb = document.querySelector('#js-moblie-select');
 dropSelect.addEventListener('change', dropDownSelect);
-
+dropSelectMb.addEventListener('change', dropDownSelect);
 function dropDownSelect(e) {
   let targetVal = e.target.value;
   switch (targetVal) {
     case '上價':
       selectDown('上價');
+
       break;
     case '中價':
       selectDown('中價');
@@ -127,9 +139,23 @@ function dropDownSelect(e) {
       break;
   }
 }
+
+//價格低到高
 function selectDown(val) {
-  data.sort((a, b) => {
+  console.log('低到高');
+
+  filterData.sort((a, b) => {
     return a[val] - b[val];
   });
-  render(data);
+  render(filterData);
+}
+//價格高到低
+
+function selectUP(val) {
+  console.log('高到低');
+
+  filterData.sort((a, b) => {
+    return b[val] - a[val];
+  });
+  render(filterData);
 }
